ffca38da078a2ad7b49c48755f72a9b22cd37190
Sheldon <sheldonchongtw@gmail.com>
2025-07-18 07:22:28 +0000

    env substitutions properly working

---
+++ srcs/requirements/mariadb/init_database.sql
@@ -0,0 +1,18 @@
+-- create database
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+
+-- create user
+/*
+In MySQL/MariaDB, users are defined as 'username'@'hostname'. The hostname part specifies where the user can connect from:
+
+'user'@'localhost' - Can only connect from the same machine
+'user'@'192.168.1.100' - Can only connect from that specific IP
+'user'@'%.example.com' - Can connect from any subdomain of example.com
+'user'@'%' - Can connect from any host/IP address
+*/
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+
+-- set user permisions
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
--- srcs/requirements/mariadb/init_maria_db.sh
+++ srcs/requirements/mariadb/init_maria_db.sh
@@ -2,27 +2,18 @@
 
 service mariadb start
 
-
-echo "test" > database.sql
-
 # Source the .env file to load environment variables
 source ./example.env
+eval "cat <<EOF >db1.sql
+$(cat init_database.sql)
+EOF"
 
-# Use the variables in a here-document
-cat <<EOF > db1.sql
--- SQL script preview:
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';
-FLUSH PRIVILEGES;
-EOF
+mysql < db1.sql
 
 # kill $(cat /var/run/mysqld/mysqld.pid)
-mysqladmin shutdown
+mysqladmin -u root -p${ROOT_PASSWORD} shutdown
 
 # this will execute mysqsld as PID 1
+# without exec, it will not be PID 1
+# use "ps -p 1" to verify that mysqld is process1
 exec mysqld
-
-# below will execute mysqld, but not as pid 1. Use "ps -p 1"
-# mysqld
