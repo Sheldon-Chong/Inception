40a46d4b34dedc53ef5678b8ba9779cd016d7953
Sheldon <sheldonchongtw@gmail.com>
2025-07-20 10:34:33 +0000

    handling wp config

--- srcs/requirements/mariadb/init_database.sql
+++ srcs/requirements/mariadb/init_database.sql
@@ -4,13 +4,12 @@ CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 -- create user
 /*
 In MySQL/MariaDB, users are defined as 'username'@'hostname'. The hostname part specifies where the user can connect from:
-
-'user'@'localhost' - Can only connect from the same machine
-'user'@'192.168.1.100' - Can only connect from that specific IP
-'user'@'%.example.com' - Can connect from any subdomain of example.com
-'user'@'%' - Can connect from any host/IP address
+	'user'@'localhost' - Can only connect from the same machine
+	'user'@'192.168.1.100' - Can only connect from that specific IP
+	'user'@'%.example.com' - Can connect from any subdomain of example.com
+	'user'@'%' - Can connect from any host/IP address
 */
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 
 -- set user permisions
