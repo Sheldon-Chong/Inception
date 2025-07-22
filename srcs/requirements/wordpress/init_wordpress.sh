775796df6b744bce9c6cd5c2a9f55e683b1bcd46
Sheldon <sheldonchongtw@gmail.com>
2025-07-22 06:09:44 +0000

    health check applied

--- srcs/requirements/wordpress/init_wordpress.sh
+++ srcs/requirements/wordpress/init_wordpress.sh
@@ -7,4 +7,14 @@ sed -i "s/username_here/${DB_USER}/" wp-config.php
 sed -i "s/password_here/${DB_USER_PASSWORD}/" wp-config.php
 sed -i "s/localhost/mariadb/" wp-config.php
 
+if ! wp core is-installed; then
+  wp core install \
+    --url="${WP_SITE_URL}" \
+    --title="${WP_SITE_TITLE}" \
+    --admin_user="${WP_ADMIN_NAME}" \
+    --admin_password="${WP_USER_PASSWORD}" \
+    --admin_email="${WP_ADMIN_EMAIL}"
+fi
+
 php -S 0.0.0.0:8000 -t /srv/www
+
